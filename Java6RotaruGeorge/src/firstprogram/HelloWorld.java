275db08084461ad86f456dbb35bc73c6406b2279
George <George@personal>
2017-09-16 17:04:25 +0300

    This is my first program in Java

---
+++ Java6RotaruGeorge/src/firstprogram/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * My frist program written in Java
+ */
+package firstprogram;
+
+/**
+ * This is my first class
+ * @author George
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // say hello world
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+    
+}
