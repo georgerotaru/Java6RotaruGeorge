c89855af512e7a68f50f7d25933ba59bafa1c8be
George <George@personal>
2017-09-28 22:31:52 +0300

    car dealer app

--- Java6RotaruGeorge/src/cardealer/CarApp.java
+++ Java6RotaruGeorge/src/cardealer/CarApp.java
@@ -12,13 +12,29 @@ package cardealer;
 public class CarApp {
 
     public static void main(String[] args) {
-        ImplementCars car1 = new ImplementCars();
-        car1.setBrand("Volkswagen");
-        car1.setType("Passat");
-        car1.setRentStatus(true);
-        car1.setRentPrice(190);
-        System.out.println("Car created = "+car1.getBrand()+" "+car1.getType()+"\n"
-        +"Rent status: "+car1.getRentStatus()+car1.getRentPrice());
+        int rentedCars = 0;
+        ImplementCars car1 = new ImplementCars("Volkswagen", "Passat", 7200, 110, false, true);
+        ImplementCars car2 = new ImplementCars("BMW", "320D", 11700, 180, true, false);
+        ImplementCars car3 = new ImplementCars("Opel", "Zafira", 3100, 40, true, false);
+        ImplementCars car4 = new ImplementCars("Mercedes", "E220", 19000, 240, false, false);
+        ImplementCars car5 = new ImplementCars("Nissan", "Juke", 8200, 120, false, false);
+        ImplementCars car6 = new ImplementCars("Ford", "Mondeo", 9700, 140, true, false);
+        ImplementCars car7 = new ImplementCars("BMW", "X5", 17000, 230, false, true);
+        ImplementCars car8 = new ImplementCars("Porsche", "Cayenne", 31000, 370, false, false);
+        ImplementCars car9 = new ImplementCars("Volkswagen", "Golf", 6300, 90, false, false);
+        ImplementCars car10 = new ImplementCars("Renault", "Laguna", 4100, 60, true, false);
+        ImplementCars car11 = new ImplementCars("Ford", "Focus", 3900, 45, false, false);
+        ImplementCars car12 = new ImplementCars("Smart", "ForFour", 5500, 80, false, true);
+
+        System.out.println("Total cars dealed: "+ImplementCars.totalCarsDealed());
+        System.out.println("Sold cars: "+ImplementCars.totalCarsSold());
+        System.out.println(ImplementCars.soldCarList());
+        System.out.println("Rented cars: "+ImplementCars.totalCarsRented());
+        System.out.println(ImplementCars.rentedCarList());
+        System.out.println("Cars in parking lot:");
+        System.out.println(ImplementCars.parkingLot());
+
+        
     }
     
 }
--- Java6RotaruGeorge/src/cardealer/ImplementCars.java
+++ Java6RotaruGeorge/src/cardealer/ImplementCars.java
@@ -7,36 +7,71 @@ package cardealer;
 
 public class ImplementCars {
 
-private String brand, type;
-int rentPrice;
-boolean rentStatus;
+private String carBrand, carModel;
+private int carSellPrice, carRentPrice;
+private boolean rentedCar, soldCar;
+private static int carsDealed, totalSoldCars, totalRentedCars;
+private static String soldCars = "";
+private static String rentedCars = "";
+private static String remainingCars = "";
 
-public ImplementCars() {
-    brand = "Default brand";
-    type = "Default brand";
+public ImplementCars(String brand, String model, int sellPrice, int rentPrice,
+        boolean rentable, boolean sold) {
+    carBrand = brand;
+    carModel = model;
+    carSellPrice = sellPrice;
+    carRentPrice = rentPrice;
+    rentedCar = rentable;
+    soldCar = sold;
+    carsDealed++;
+
+    if (soldCar == true)
+        totalSoldCars++;
+    if (soldCar == true)
+        soldCars += "Car sold: "+carBrand+" "+carModel+" for :"+carSellPrice+" euro\n";
+    if (rentedCar == true)
+        totalRentedCars++;
+    if (rentedCar == true)
+        rentedCars += "Car rented: "+carBrand+" "+carModel+" for :"+carRentPrice+" euro per day\n";
+    if (rentedCar == false && soldCar == false)
+        remainingCars += "Brand: "+carBrand+", Model "+carModel+", rent price: "
+                +carRentPrice+" euro, selling price: "+carSellPrice+" euro.\n";
 }
 
 public String getBrand() {
-    return(brand);
+    return(carBrand);
 }
-
-public void setBrand(String newBrand) {
-    brand = newBrand;
+public String getModel() {
+    return(carModel);
 }
-
-public String getType() {
-    return(type);
+public int sellingPrice() {
+    return(carSellPrice);
 }
-
-public void setType(String newType) {
-    type = newType;
+public int rentingPrice() {
+    return(carRentPrice);
 }
-
-public int getRentPrice() {
-    return(rentPrice);
+public boolean rentableCar() {
+    return(rentedCar);
 }
-
-public void setRentPrice(int newRentPrice) {
-    rentPrice = newRentPrice;
+public boolean carSold() {
+    return(soldCar);
+}
+public static int totalCarsDealed() {
+    return(carsDealed);
+}
+public static int totalCarsSold() {
+    return(totalSoldCars);
+}
+public static int totalCarsRented() {
+    return(totalRentedCars);
+}
+public static String soldCarList() {
+    return(soldCars);
+}
+public static String rentedCarList() {
+    return(rentedCars);
+}
+public static String parkingLot() {
+    return(remainingCars);
 }
 }
